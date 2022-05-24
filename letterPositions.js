14b768ab8a529a93bafcef56d6e18cb7e5d21fcf
Paul Taylor <paul@whammydiver.com>
2022-05-23 23:34:29 -0400

    test codes added

--- letterPositions.js
+++ letterPositions.js
@@ -1,19 +1,36 @@
 const letterPositions = function(sentence) {
   const results = {};
   sentence = sentence.split(" ").join("");
-  for (let char in sentence) {
-    if (results[sentence[char]]) {
-      results[sentence[char]].push(char);
+  for (let x = 0; x < sentence.length; x++) {
+    if (results[sentence[x]]) {
+      results[sentence[x]].push(x);
+      console.log(sentence[x]);
     } else {
-      results[sentence[char]] = [char];
+      results[sentence[x]] = [x];
     }
   }
-  
-
-
-
   return results;
 };
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅✅✅ Assertion Passed: arrays are identical");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: arrays do NOT match");
+  }
+}
 
 let sentence = process.argv.slice(2);
 console.log(sentence);
@@ -23,5 +40,13 @@ for (let x = 0; x < sentence.length; x++) {
 }
 console.log(letterPositions(sentenceString));
 
-//sentence.join();
-//console.log(letterPositions(sentence));
+// TEST CODES
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("goodbye").o, [1, 2]);
+assertArraysEqual(letterPositions("bexley").e, [1, 4]);
+assertArraysEqual(letterPositions("ranji").j, [3]);
+assertArraysEqual(letterPositions("monkman").m, [0, 4]);
+assertArraysEqual(letterPositions("cheeks").e, [2, 3]);
+assertArraysEqual(letterPositions("charlie").e, [6]);
+assertArraysEqual(letterPositions("paul").a, [1]);
+assertArraysEqual([1,2,3], [1,2,3]);
