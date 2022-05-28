1d7caa5fc2951b984a39e59ca094d505da0b46d0
Paul Taylor <paul@whammydiver.com>
2022-05-28 16:32:51 -0400

    first commit takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const takeUntil = function(array, callback) {
+  results = [];
+  for (item of array) {
+    if (!callback(item)) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+}
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅✅✅ Assertion Passed: arrays are identical");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: arrays do NOT match");
+  }
+}
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
