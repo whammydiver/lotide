787b92cf3db7a2d52c4f8fbb35fa018616ef2dcb
Paul Taylor <paul@whammydiver.com>
2022-06-08 15:19:49 -0600

    refactro assertArraysEqual and added seperate test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,42 +1,11 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let x = 0; x < array1.length; x++) {
-    if (array1[x] !== array2[x]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log("✅✅✅ Assertion Passed: arrays are identical");
+    console.log("✅✅✅ Assertion Passed: Arrays match");
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: arrays do NOT match");
+    console.log("🛑🛑🛑 Assertion Failed: Arrays do NOT match.");
   }
 }
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual(2, 5);
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require('../assertArraysEqual.js');
+const eqArrays = require('../eqArrays.js');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
