1b72dfcea5fb343582fbfe5e3b439b526ce7b007
Paul Taylor <paul@whammydiver.com>
2022-06-11 16:24:23 -0400

    remove pigLatin and reverse function files

--- reverse.js
+++
@@ -1,9 +0,0 @@
-const reverse = function(word) {
-  let reversedWord = "";
-  for (let x = word.length - 1; x >= 0; x--) {
-    reversedWord += word[x];
-  }
-  return reversedWord;
-};
-
-module.exports = reverse;
